1505470d1a5753f45138b3e3c8f423fcc1c1e914
Thomas Atkinson <thomasatv@yahoo.co.uk>
2023-11-20 15:18:14 +0000

    merge blip from task one (approved)

--- app.js
+++ app.js
@@ -5,18 +5,11 @@ const { getTopics } = require('./controllers/controller.topics')
 
 const {
     invalidEndpoint,
-    handleCustomErrors,
-    handlePsqlErrors,
-    handleServerErrors,
   } = require('./controllers/controller.errors');
 
 app.get('/api/topics', getTopics);
 
-app.all("*", invalidEndpoint); // do i need this error yet?
-
-app.use(handleCustomErrors);
-app.use(handlePsqlErrors);
-app.use(handleServerErrors);
+app.all("*", invalidEndpoint);
 
 module.exports = app;
 
