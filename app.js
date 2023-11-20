332cdc1105195da8ed9224852be38dabaa4a8514
Thomas Atkinson <thomasatv@yahoo.co.uk>
2023-11-20 16:41:51 +0000

    task 4 ready for pull request

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 
 const { getTopics } = require('./controllers/controller.topics')
-
+const { getArticleById } = require('./controllers/controller.aritcles')
 const {
     invalidEndpoint,
+    handleCustomErrors,
+    handlePsqlErrors
   } = require('./controllers/controller.errors');
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleById)
 
 app.all("*", invalidEndpoint);
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
 
 module.exports = app;
 
